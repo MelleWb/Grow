323c0ab5ee9e21fd6a6fe5290fbc0db909dfc327
swenrolink <swen.rolink@labbconsulting.com>
2021-06-11 19:37:33 +0200

    Initial Commit

---
+++ Grow/GrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrowApp.swift
+//  Grow
+//
+//  Created by Swen Rolink on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct GrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
